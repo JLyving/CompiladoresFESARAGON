1babc0563f810ea202d6f936f7b3914ca07047c2
JUANIMAN <84895566+JUANIMAN@users.noreply.github.com>
2023-03-30 18:10:37 -0600

    Programa 4
    Lee palabras de un archivo fuente que empiece con una Letra, pueda tener o no Dígitos y termine con ":", con una tabla de transiciones configurable.

---
+++ PruebaDos/src/fes/aragon/inicio/PruebaDos.java
@@ -0,0 +1,89 @@
+package fes.aragon.inicio;
+
+import fes.aragon.util.Utilerias;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PruebaDos {
+	private char[] columnas;
+	private int[][] tabla;
+	private int estado = 0;
+	private int columna = 0;
+
+	public static void main(String[] args) {
+		PruebaDos app = new PruebaDos();
+		try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/config"))) {
+			String[] valores = br.readLine().split(",");
+			int numColumnas = Integer.parseInt(valores[0]);
+			int numEstados = Integer.parseInt(valores[1]);
+
+			app.columnas = new char[numColumnas];
+			valores = br.readLine().split(",");
+			for (int i = 0; i < numColumnas; i++) {
+				app.columnas[i] = valores[i].charAt(0);
+			}
+
+			app.tabla = new int[numEstados][numColumnas];
+			for (int i = 0; i < numEstados; i++) {
+				valores = br.readLine().split(",");
+				for (int j = 0; j < numColumnas; j++) {
+					app.tabla[i][j] = Integer.parseInt(valores[j]);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			archivo = new File(System.getProperty("user.dir") + "/fuente");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				for (String token : linea.split(" ")) {
+					app.estado = 0;
+					for (int i = 0; i < token.length(); i++) {
+						char simbolo = token.charAt(i);
+						if (Utilerias.letra(simbolo)) {
+							simbolo = 'L';
+						} else if (Utilerias.digito(simbolo)) {
+							simbolo = 'D';
+						}
+						for (int j = 0; j < app.columnas.length; j++) {
+							if (app.columnas[j] == simbolo) {
+								app.columna = j;
+								break;
+							}
+						}
+						app.estado = app.tabla[app.estado][app.columna];
+					}
+					if (app.estado == 1 && token.charAt(token.length() - 1) == ':') {
+						System.out.println(token + " = Aceptada");
+					} else {
+						System.out.println(token + " = No aceptada");
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+}
